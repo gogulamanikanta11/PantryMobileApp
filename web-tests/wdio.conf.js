7f6d159d49bb35fa0e1d8f6a8d4ba5433e217592
Gogula Manikanta <gogulamanikanta8@gmail.com>
2026-06-13 12:44:10 +0530

    Configure WebdriverIO to use automatic browser driver management and remove chromedriver dependencies

--- web-tests/wdio.conf.js
+++ web-tests/wdio.conf.js
@@ -17,7 +17,7 @@ exports.config = {
     waitforTimeout: 10000,
     connectionRetryTimeout: 120000,
     connectionRetryCount: 3,
-    services: ['chromedriver'],
+    services: [],
     framework: 'mocha',
     reporters: ['spec'],
     mochaOpts: {
