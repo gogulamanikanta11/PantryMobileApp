be15817535665076358c5850f96ed4b2503d8ee9
Gogula Manikanta <gogulamanikanta8@gmail.com>
2026-07-26 11:40:59 +0530

    fix: optimize emulator runner configuration to prevent timeout

--- appium-tests/wdio.conf.js
+++ appium-tests/wdio.conf.js
@@ -13,7 +13,7 @@ exports.config = {
   capabilities: [{
     platformName: 'Android',
     'appium:deviceName': 'emulator-5554',         // Match your adb devices output
-    'appium:platformVersion': '13',               // Update to match your emulator version
+    'appium:platformVersion': '11',               // Update to match your emulator version
     'appium:app': path.resolve(__dirname, '../android/app/build/outputs/apk/debug/app-debug.apk'),
     'appium:automationName': 'UiAutomator2',
     'appium:appPackage': 'com.gmanikanta.pantrymobileapp', // Updated to match your app.json
